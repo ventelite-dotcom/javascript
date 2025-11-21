63765f9286cb5674aed491377839dad029b5b472
Yash Gupta <yashgupta8910890643@gmail.com>
2025-11-21 14:19:37 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144533
+let accountEmail = "yashgupta8910890643@gmail.com"
+var accountPassword =  "12345"
+accountCity = "Kolkata"
+
+// accountId = 2 // cannot be changed since constant
+
+accountEmail = "dj@dj.com"
+accountPassword = "56133"
+accountCity = "delhi"
+
+console.log(accountId);
+
+console.table({accountEmail,accountId,accountPassword,accountCity})
